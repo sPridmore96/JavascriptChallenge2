e2f869787642ab0f00cb35a807897e8f8e66260d
sam <sam_pridmore@hotmail.co.uk>
2022-05-08 14:48:27 +0100

    attempted to make days to years more realistic with accounting for leap years. Made a reuseable function for getElemantByClass(only for individual numbers). Finished sCss

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-notWorking = "This is not working"
+notWorking = "This is not working";
+
+let elementByClassParseFloat = (className = '') => {
+
+    return parseFloat(document.getElementsByClassName(className)[0].value);
+}
 
 // A function to check if a number is positive or negative
 
 const checkNumberPositivty = () => {
 
-    let userNumber = document.getElementById('userNumber').value
+    let userNumber = elementByClassParseFloat('userNumber');
+
 
     if (userNumber >= 0) {
         return "positive!"
@@ -18,30 +24,47 @@ const checkNumberPositivty = () => {
 // A function to convert a number of days into year (rounds to the nearest year)
 
 const handleCalculateAge = () => {
-    let totalDays = document.getElementById('totalDays').value
-    const calculatedAge = totalDays / 365;
-    const message = `The total days entered " ${totalDays} " roughly equate to ${Math.round(calculatedAge)} Years`
-    return message
+    let totalDays = parseFloat(document.getElementById('totalDays').value)
+
+    let calculateYears = totalDays / 365
+    let calculateTheDays = totalDays % 365;
+    const leepYearCounter = Math.floor(calculateYears / 4);
+    let calculatedDaysWithLeapYrs = calculateTheDays + leepYearCounter;
+
+    if (totalDays < 365) {
+        return `${totalDays} days equate to 0 years and ${totalDays} days`
+    } else if (calculatedDaysWithLeapYrs > 365) {
+        calculateYears =  calculateYears + Math.floor(calculatedDaysWithLeapYrs / 365);
+        calculatedDaysWithLeapYrs = Math.floor(calculatedDaysWithLeapYrs % 365);
+        return `${totalDays} days equate to ${Math.floor(calculateYears)} Years and ${Math.floor(calculatedDaysWithLeapYrs)} days`
+    } else if (calculatedDaysWithLeapYrs == 365) {
+        calculateYears++
+        return `${totalDays} days equate to ${Math.floor(calculateYears)} Years and 0 days`
+    }else if (totalDays == 4 * 365){
+        return `${calculateYears} years and 365 Days (It'd be a leap Year)`
+    } else {
+        return `${totalDays} days equate to ${Math.floor(calculateYears)} Years and ${Math.floor(calculatedDaysWithLeapYrs)} days`
+    }
 }
+
+
 
 //a function to test 3 values and display the highest
 
 const handleValueChecker = () => {
-    let inputValue1 = parseFloat(document.getElementById('userValue1').value);
-    let inputValue2 = parseFloat(document.getElementById('userValue2').value);
-    let inputValue3 = parseFloat(document.getElementById('userValue3').value);
+    let inputValue1 = parseInt(document.getElementById('userValue1').value);
+    let inputValue2 = parseInt(document.getElementById('userValue2').value);
+    let inputValue3 = parseInt(document.getElementById('userValue3').value);
 
     valuesToCheckArray = [inputValue1, inputValue2, inputValue3];
 
     let highest = valuesToCheckArray[0];
 
-    valuesToCheckArray.forEach(valueChecker)
-
-    function valueChecker(num) {
+    valuesToCheckArray.forEach(num => {
         if (num > highest) {
             highest = num;
         }
-    }
+    })
     return `The Highest number entered is ${highest}`;
 }
 
@@ -49,20 +72,24 @@ const handleValueChecker = () => {
 
 const getLastNames = () => {
 
-    let inputName1 = document.getElementById('userName1').value;
-    let inputName2 = document.getElementById('userName2').value;
-    let inputName3 = document.getElementById('userName3').value;
-    let inputName4 = document.getElementById('userName4').value;
+    let inputName1 = document.getElementById('userName1').value.split(" ");
+    let inputName2 = document.getElementById('userName2').value.split(" ");
+    let inputName3 = document.getElementById('userName3').value.split(" ");
+    let inputName4 = document.getElementById('userName4').value.split(" ");
 
     let namesToCheck = [inputName1, inputName2, inputName3, inputName4];
-    return namesToCheck[namesToCheck.length - 1];
+    let lastName = [];
+    let allLastNames = [];
+
+    namesToCheck.forEach(arr => {
+        lastName = arr.slice(arr.length - 1);
+        allLastNames.push(" " + lastName)
+        message = `all the last names are: ${allLastNames}`
+    })
+    return message;
 }
-
-
-
 // a function that takes a list of numbers, will check if all are positive 
 // or if there are any negatives and return a statment to show this
-
 
 const updateListOfArrays = () => {
     let listOfArrays = document.getElementById('listOfArrays');
@@ -73,16 +100,15 @@ const checkNumberPositivtyOfList = () => {
 
     const selectedArray = updateListOfArrays();
 
-    let positivity = "positive!";
+    let positivity = "True";
     let positiveCount = selectedArray.length;
     let negativeCount = 0;
-
-    selectedArray.forEach(num => {
+    selectedArray.some(num => {
         num = parseInt(num)
         if (num < 0) {
             negativeCount++;
             positiveCount--;
-            num = "negative!";
+            num = "False";
             positivity = num;
         }
     })
